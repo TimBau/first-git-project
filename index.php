f160d0dd0c604c9353e645ec41c295f3f98ebe17
Tim Bauer <timbauer35@gmail.com>
2016-11-09 13:17:35 +0100

    mol

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <h1>PHP Grundlagen</h1>
+    <p>
+      Einführung
+    </p>
+      <?php
+      $vorname = 'Tim';
+      $nachname = 'Bauer';
+      $alter = 30;
+      $grösse = 1.77;
+
+      echo 'Hallo zusammen, ich bin'. $vorname .' '. $nachname .'und ich bin '. $alter .' Jahre alt, sowie '. $grösse .' Meter gross.'
+
+       ?>
+
+  </body>
+</html>
